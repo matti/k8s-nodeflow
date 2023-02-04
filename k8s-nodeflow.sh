4ee6678eb9c9acd87e63ba681c2256993d459e41
Matti Paksula <matti.paksula@iki.fi>
2023-02-04 17:14:55 +0200

    multi labels

--- k8s-nodeflow.sh
+++ k8s-nodeflow.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -Euo pipefail
 
+_term() {
+  echo "TERM"
+  exit 0
+}
+
+trap _term INT TERM
+
 K8S_NODEFLOW_DEBUG=${K8S_NODEFLOW_DEBUG:-no}
 K8S_NODEFLOW_NODES_MINIMUM=${K8S_NODEFLOW_NODES_MINIMUM:-0}
 
@@ -11,20 +18,33 @@ function _log() {
 
 drain_all_in=$1
 drain_nodes_with_this_label=$2
-pods_not_running_by_this_label_prevent_draining=$3
+pods_not_running_by_these_labels_prevent_draining=${*:3}
+
+_log "drain_all_in: $drain_all_in"
+_log "drain_nodes_with_this_label: $drain_nodes_with_this_label"
+_log "pods_not_running_by_these_labels_prevent_draining: $pods_not_running_by_these_labels_prevent_draining"
 
 while true; do
-  while true; do
-    pods_not_running=$(2>/dev/null kubectl get pod --no-headers \
-      --all-namespaces \
-      -l "$pods_not_running_by_this_label_prevent_draining" --field-selector=status.phase!=Running) || true
+  for pods_not_running_by_this_label_prevents_draining in $pods_not_running_by_these_labels_prevent_draining
+  do
+    (
+      while true; do
+        pods_not_running=$(2>/dev/null kubectl get pod --no-headers \
+          --all-namespaces \
+          -l "$pods_not_running_by_this_label_prevents_draining" --field-selector=status.phase!=Running) || true
 
-    [[ "$pods_not_running" == "" ]] && break
+        [[ "$pods_not_running" == "" ]] && break
 
-    pods_not_running_count=$(echo "$pods_not_running" | wc -l | xargs) || true
-    _log "draining paused, '$pods_not_running_count' pods with label '$pods_not_running_by_this_label_prevent_draining' not running"
+        pods_not_running_count=$(echo "$pods_not_running" | wc -l | xargs) || true
+        _log "draining paused, '$pods_not_running_count' pods with label '$pods_not_running_by_this_label_prevents_draining' not running"
 
-    sleep 3
+        sleep 3
+      done
+
+      echo "all pods with label $pods_not_running_by_this_label_prevents_draining are running"
+    ) &
+
+    wait
   done
 
   # start counting after not paused
